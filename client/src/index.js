9d1006164acfa0dac067931495cc0cbfe702b06b
JavierVRM <javara00@gmail.com>
2022-06-06 23:58:26 +0200

    Raw styling (missing variables)

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,3 @@
 import PlayerCard from './player-card/playerCard.js'
 
-window.customElements.define("player-card", PlayerCard);
+customElements.define("player-card", PlayerCard);
--- client/src/player-card/playerCard.js
+++ client/src/player-card/playerCard.js
@@ -1,29 +1,5 @@
 import globalMethods from '../global-methods/globalMethods.js'
 
-const template = document.createElement('template')
-template.innerHTML = `
-<section class="player__card__container">
-    <select class="player__card__selector">
-    </select>
-    <div class="player__card__images">
-        <img class="player__photo__img">
-        <div class="player__club">
-            <img src="" alt="" class="player__club__img">
-        </div>
-    </div>
-    <div class="player__card__info">
-        <div class="player__name"></div>
-        <div class="player__position"></div>
-        <ul class="player__stats">
-            <li class="player__stat appearances"></li>
-            <li class="player__stat goals"></li>
-            <li class="player__stat assists"></li>
-            <li class="player__stat goalspm"></li>
-            <li class="player__stat passespm"></li>
-        </ul>
-    </div>
-</section>
-`
 
 class PlayerCard extends HTMLElement {
     playerClub = ''
@@ -35,11 +11,11 @@ class PlayerCard extends HTMLElement {
     playerAssists = ''
     playerGoalsPm = ''
     playerPassesPm = ''
-    
+
     constructor() {
         super()
         this.attachShadow({ mode:'open' })
-        this.shadowRoot.appendChild(template.content.cloneNode(true))
+
 
         const playersRawData = globalMethods.fetchData('http://localhost:3000/data')
         const playerData = {}
@@ -49,6 +25,54 @@ class PlayerCard extends HTMLElement {
             }
         })
         
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    get style() {
+        return `<style>@import '/style.css' </style>`
+    }
+
+    get template () {
+        return `<section class="player__card__container">
+            
+            <div class="player__card__images">
+                <select class="player__card__selector">
+                <option>Select bla</option>
+                <option>Hristo Stoichkov</option>
+                </select>
+                <img class="player__photo__img" src='https://tmssl.akamaized.net/images/foto/galerie/hristo-stoichkov-bei-barcelona-1575285527-27908.jpg?lm=1575285533'>
+                <img class="player__club__img" src="https://tmssl.akamaized.net/images/foto/galerie/hristo-stoichkov-bei-barcelona-1575285527-27908.jpg?lm=1575285533" alt="" class="player__club__img">
+            </div>
+            <div class="player__card__info">
+                <div class="player__name">Hristo Stoichkov</div>
+                <div class="player__position">Striker</div>
+                <ul class="player__stats">
+                    <li class="player__stat appearances">
+                    <span>appearances</span><span>10</span>
+                    </li>
+                    <li class="player__stat goals">
+                    <span>appearances</span><span>10</span>
+                    </li>
+                    <li class="player__stat assists">
+                    <span>appearances</span><span>10</span>
+                    </li>
+                    <li class="player__stat goalspm">
+                    <span>appearances</span><span>10</span>
+                    </li>
+                    <li class="player__stat passespm">
+                    <span>appearances</span><span>10</span>
+                    </li>
+                </ul>
+            </div>
+        </section>
+        `
+    }
+
+    render() {
+        this.shadowRoot.innerHTML = `${this.style}${this.template}`
     }
 }
 
